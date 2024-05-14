8390c244bd349e8219b252e11010980e1abef620
Gabe <gabehenley517@hotmail.com>
2024-05-14 10:47:04 -0400

    refactoring

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,10 @@
 extends CharacterBody2D
 
 @onready var player = $AnimatedSprite2D
-@onready var dash_cooldown = %dash_cooldown
-@onready var dashtimer = %dashtimer
+@onready var dash_cooldown_timer = %dash_cooldown_timer
+@onready var dash_duration = %dash_duration
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var dashcooldowntimer = $dashcooldown
-@onready var dashcooldown = $dash_cooldown
-
+@onready var dash_cooldown = %dash_cooldown
 
 
 const SPEED = 200.0
@@ -31,10 +29,10 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("dash") and can_dash:
 			dash = true
 			can_dash = false
-			dashtimer.start()
-			dash_cooldown.start()
-			dashcooldown.visible = true
-			dashcooldown.start_cooldown(DASH_COOLDOWN)
+			dash_duration.start()
+			dash_cooldown_timer.start()
+			dash_cooldown.visible = true
+			dash_cooldown.start_cooldown(DASH_COOLDOWN)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -64,11 +62,11 @@ func _physics_process(delta):
 	move_and_slide()
 	
 
-func _on_dash_cooldown_timeout():
-	can_dash = true
-	dashcooldown.visible = false
-	dashcooldown.reset()
-
-
-func _on_dashtimer_timeout():
+func _on_dash_duration_timeout():
 	dash = false
+
+func _on_dash_cooldown_timer_timeout():
+	can_dash = true
+	dash_cooldown.visible = false
+	dash_cooldown.reset()
+
