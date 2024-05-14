ae9f6c95cf84f0d8c32bedd48bd2f932323536ff
Luke Classen <lukeyclassen@gmail.com>
2024-05-13 23:52:45 -0400

    various small changes to player + camera

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -11,7 +11,7 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -500.0
-const SUPER_JUMP_VELOCITY = -800.0
+const SUPER_JUMP_VELOCITY = -700.0
 const DASH_SPEED = 400
 const DASH_COOLDOWN = 3
 
@@ -42,8 +42,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("super_jump") and is_on_floor():
 		velocity.y = SUPER_JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		if dash:
